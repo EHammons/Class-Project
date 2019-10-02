a441f7ed0c83fc4323383cabedf18cfe49b30054
Stardust <52759497+EHammons@users.noreply.github.com>
2019-10-02 14:11:09 -0500

    added gitignore and edited cli a bit

--- cli.js
+++ cli.js
@@ -1,15 +1,21 @@
-// Write code here to parse command line arguments and store them into variables
-// Add code to print whether the user is searching for an actor or tv show, along with the name of the actor or tv show they are searching for
+var TV = require("./tv.js");
 
 var choice = process.argv[2];
 var subject = process.argv.slice(3).join(" ");
+var show;
+var actor;
 
 if (choice === "show") {
     console.log("Your choose a " + choice);
-    console.log(subject);
+    show = subject;
+    console.log(show);
 } else if (choice === "actor") {
     console.log("Your choose an " + choice);
-    console.log(subject);
+    actor = subject;
+    console.log(actor);
 } else {
     console.log("You did not request a show or an actor.\nPlease choose again in the following format: \nnode cli.js actor Brad Pitt\nnode cli.js show The Simpsons")
 }
+
+var tv = new TV();
+tv.findShow(show);

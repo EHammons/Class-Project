b5f8fc723692ab032e90f22bc4d847b8911be79f
Stardust <52759497+EHammons@users.noreply.github.com>
2019-10-02 13:25:52 -0500

    begining cli coding

--- cli.js
+++ cli.js
@@ -1,2 +1,6 @@
 // Write code here to parse command line arguments and store them into variables
 // Add code to print whether the user is searching for an actor or tv show, along with the name of the actor or tv show they are searching for
+
+var choice = process.argv[2];
+var subject = process.argv.slice(3).join(" ");
+console.log(subject + " is a type of " + choice);
